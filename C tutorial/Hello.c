7424277b1afadf56e2264b873cb7229899fb0a93
Apoorv <asnic3354@gmail.com>
2024-05-27 00:24:19 +0530

    Hello.c

--- C tutorial/Hello.c
+++ C tutorial/Hello.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
-
+ #include <stdio.h>
 int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+   int a, b;
+   printf("enter a");
+   scanf("%d", &a);
+
+   printf("enter b");
+   scanf("%d", &b); 
+
+   int sum = a + b;
+   printf("sum is : %d", sum);
+   return 0;
+   }
